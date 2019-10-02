6695358e0b41a5a472c20b64502d01e2adf010c9
Ashley Norton <nortonashleym@gmail.com>
2019-10-02 02:57:45 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,4 +8,11 @@ attr_accessor :name, :csv_emails
     @csv_emails = csv_emails
   end 
   
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+  
 end 
